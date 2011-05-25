3888dfd60b33154a7ad7a087261c0004fa8af6e5
Lauro Neto <lauro.neto@openbossa.org>
2011-05-25 15:53:49 -0300

    Initial tests for name-related methods

---
+++ tests/testnaming.cpp
@@ -0,0 +1,263 @@
+/*
+* This file is part of the API Extractor project.
+*
+* Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+*
+* Contact: PySide team <contact@pyside.org>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA
+*
+*/
+
+#include "testnaming.h"
+#include "abstractmetabuilder.h"
+#include <QtTest/QTest>
+#include "testutil.h"
+
+/** Basic rules of naming
+ *
+ *  AbstractMeta*->name() -> Target language name, WITHOUT scope information
+ *  TypeEntry->name() -> C++ name, WITHOUT scope information
+ *  TypeEntry->qualifiedCppName() -> Self explanatory
+ *  TypeEntry->targetLangName() -> Target language name, WITH scope but WITHOUT package
+ *  TypeEntry->qualifiedTargetLangName() -> Target language name, WITH scope and WITH package
+ */
+
+void TestNaming::testClass()
+{
+    const char* cppCode ="class ClassName {};";
+    const char* xmlCode = "<typesystem package=\"Foo\"><value-type name=\"ClassName\"/></typesystem>";
+    TestUtil t(cppCode, xmlCode);
+    AbstractMetaClassList classes = t.builder()->classes();
+    QCOMPARE(classes[0]->name(), QString("ClassName"));
+
+    TypeEntry* type = classes[0]->typeEntry();
+    QVERIFY(type);
+    QCOMPARE(type->name(), QString("ClassName"));
+    QCOMPARE(type->qualifiedCppName(), QString("ClassName"));
+    QCOMPARE(type->targetLangName(), QString("ClassName"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.ClassName"));
+}
+
+void TestNaming::testClassInNamespace()
+{
+    const char* cppCode ="namespace Nsp { class ClassName {}; }";
+    const char* xmlCode = "<typesystem package=\"Foo\"><namespace-type name=\"Nsp\">\
+                           <value-type name=\"ClassName\"/></namespace-type></typesystem>";
+    TestUtil t(cppCode, xmlCode);
+    AbstractMetaClassList classes = t.builder()->classes();
+    AbstractMetaClass* a = classes.findClass("ClassName");
+    QVERIFY(a);
+    QCOMPARE(a->name(), QString("ClassName"));
+
+    TypeEntry* type = a->typeEntry();
+    QVERIFY(type);
+    QCOMPARE(type->name(), QString("ClassName"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::ClassName"));
+    QCOMPARE(type->targetLangName(), QString("Nsp.ClassName"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp.ClassName"));
+
+    AbstractMetaClass* b = classes.findClass("Nsp");
+    QVERIFY(b);
+    QCOMPARE(b->name(), QString("Nsp"));
+
+    type = b->typeEntry();
+    QVERIFY(type);
+    QCOMPARE(type->name(), QString("Nsp"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp"));
+    QCOMPARE(type->targetLangName(), QString("Nsp"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp"));
+}
+
+void TestNaming::testClassInHiddenNamespace()
+{
+    const char* cppCode ="namespace Nsp { class ClassName {}; }";
+    const char* xmlCode = "<typesystem package=\"Foo\"><namespace-type name=\"Nsp\" generate=\"no\">\
+                           <value-type name=\"ClassName\"/></namespace-type></typesystem>";
+    TestUtil t(cppCode, xmlCode);
+    AbstractMetaClassList classes = t.builder()->classes();
+    AbstractMetaClass* a = classes.findClass("ClassName");
+    QVERIFY(a);
+    QCOMPARE(a->name(), QString("ClassName"));
+
+    TypeEntry* type = a->typeEntry();
+    QVERIFY(type);
+    QCOMPARE(type->name(), QString("ClassName"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::ClassName"));
+    QCOMPARE(type->targetLangName(), QString("ClassName"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.ClassName"));
+
+    // Test name of non-generated namespace
+    AbstractMetaClass* b = classes.findClass("Nsp");
+    QVERIFY(b);
+    QCOMPARE(b->name(), QString(""));
+
+    type = b->typeEntry();
+    QVERIFY(type);
+    QVERIFY(!type->generateCode());
+
+    QCOMPARE(type->name(), QString("Nsp"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp"));
+    QCOMPARE(type->targetLangName(), QString(""));
+    QCOMPARE(type->qualifiedTargetLangName(), QString(""));
+}
+
+void TestNaming::testEnum()
+{
+    const char* cppCode ="enum GlobalEnum { A, B};\
+                          struct A {\
+                              enum ClassEnum { A, B };\
+                              void method(ClassEnum);\
+                          };\
+                          void func(A::ClassEnum);";
+    const char* xmlCode = "\
+    <typesystem package=\"Foo\"> \
+        <enum-type name='GlobalEnum' />\
+        <value-type name='A'> \
+            <enum-type name='ClassEnum' />\
+        </value-type> \
+        <function signature='func(A::ClassEnum)' />\
+    </typesystem>";
+
+    TestUtil t(cppCode, xmlCode);
+
+    // Global enum
+    AbstractMetaEnumList enums = t.builder()->globalEnums();
+    AbstractMetaEnum* e = enums.first();
+    QCOMPARE(e->name(), QString("GlobalEnum"));
+
+    TypeEntry* type = e->typeEntry();
+    QCOMPARE(type->name(), QString("GlobalEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("GlobalEnum"));
+    QCOMPARE(type->targetLangName(), QString("GlobalEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.GlobalEnum"));
+
+    // Class Enum
+    AbstractMetaClassList classes = t.builder()->classes();
+    e = classes.first()->enums().first();
+    QCOMPARE(e->name(), QString("ClassEnum"));
+
+    type = e->typeEntry();
+    QCOMPARE(type->name(), QString("ClassEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("A::ClassEnum"));
+    QCOMPARE(type->targetLangName(), QString("A.ClassEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.A.ClassEnum"));
+
+    // TODO test the AbstractMetaType
+}
+
+void TestNaming::testEnumInNamespace()
+{
+    const char* cppCode ="namespace Nsp {\
+                            enum GlobalEnum { a, b};\
+                            struct A {\
+                                enum ClassEnum { A, B };\
+                                void method(ClassEnum);\
+                            };\
+                            void func(A::ClassEnum);\
+                          }";
+    const char* xmlCode = "\
+    <typesystem package=\"Foo\"> \
+        <namespace-type name='Nsp'>\
+            <enum-type name='GlobalEnum' />\
+            <value-type name='A'> \
+                <enum-type name='ClassEnum' />\
+            </value-type> \
+            <function signature='func(A::ClassEnum)' />\
+        </namespace-type>\
+    </typesystem>";
+
+    TestUtil t(cppCode, xmlCode);
+
+    // Global enum
+    AbstractMetaClassList classes = t.builder()->classes();
+    AbstractMetaClass* nsp = classes.findClass("Nsp");
+    AbstractMetaEnum* e = nsp->enums().first();
+    QCOMPARE(e->name(), QString("GlobalEnum"));
+
+    TypeEntry* type = e->typeEntry();
+    QCOMPARE(type->name(), QString("GlobalEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::GlobalEnum"));
+    QCOMPARE(type->targetLangName(), QString("Nsp.GlobalEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp.GlobalEnum"));
+
+    // Class Enum
+    AbstractMetaClass* a = classes.findClass("Nsp::A");
+    QVERIFY(a);
+    e = a->enums().first();
+    QCOMPARE(e->name(), QString("ClassEnum"));
+
+    type = e->typeEntry();
+    QCOMPARE(type->name(), QString("ClassEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::A::ClassEnum"));
+    QCOMPARE(type->targetLangName(), QString("Nsp.A.ClassEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp.A.ClassEnum"));
+
+    // TODO test the AbstractMetaType
+}
+
+void TestNaming::testEnumInHiddenNamespace()
+{
+    const char* cppCode ="namespace Nsp {\
+                            enum GlobalEnum { a, b};\
+                            struct A {\
+                                enum ClassEnum { A, B };\
+                                void method(ClassEnum);\
+                            };\
+                            void func(A::ClassEnum);\
+                          }";
+    const char* xmlCode = "\
+    <typesystem package=\"Foo\"> \
+        <namespace-type name='Nsp'>\
+            <enum-type name='GlobalEnum' />\
+            <value-type name='A'> \
+                <enum-type name='ClassEnum' />\
+            </value-type> \
+            <function signature='func(A::ClassEnum)' />\
+        </namespace-type>\
+    </typesystem>";
+
+    TestUtil t(cppCode, xmlCode);
+
+    // Global enum
+    AbstractMetaClassList classes = t.builder()->classes();
+    AbstractMetaClass* nsp = classes.findClass("Nsp");
+    AbstractMetaEnum* e = nsp->enums().first();
+    QCOMPARE(e->name(), QString("GlobalEnum"));
+
+    TypeEntry* type = e->typeEntry();
+    QCOMPARE(type->name(), QString("GlobalEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::GlobalEnum"));
+    QCOMPARE(type->targetLangName(), QString("Nsp.GlobalEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp.GlobalEnum"));
+
+    // Class Enum
+    AbstractMetaClass* a = classes.findClass("Nsp::A");
+    QVERIFY(a);
+    e = a->enums().first();
+    QCOMPARE(e->name(), QString("ClassEnum"));
+
+    type = e->typeEntry();
+    QCOMPARE(type->name(), QString("ClassEnum"));
+    QCOMPARE(type->qualifiedCppName(), QString("Nsp::A::ClassEnum"));
+    QCOMPARE(type->targetLangName(), QString("Nsp.A.ClassEnum"));
+    QCOMPARE(type->qualifiedTargetLangName(), QString("Foo.Nsp.A.ClassEnum"));
+
+    // TODO test the AbstractMetaType
+}
+
+QTEST_APPLESS_MAIN(TestNaming)
+
+#include "testnaming.moc"
